83f67b6ad9aa28ad12ac0a4bf9d4781591fc420d
shizz <707325903@qq.com>
2020-12-09 00:36:58 +0800

    first

---
+++ src/pages/demo/index.tsx
@@ -0,0 +1,15 @@
+import { PageHeaderWrapper } from "@ant-design/pro-layout";
+import React, { Component } from "react";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Index extends Component{
+    render(){
+        return(
+            <PageHeaderWrapper>
+                <div>sasd</div>
+            </PageHeaderWrapper>
+        )
+    }
+}
+
+export default Index;
